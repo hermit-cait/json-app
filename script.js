8729c8ea0062748d6795e4de838c5ee61e2614ee
hermit-cait <63665275+hermit-cait@users.noreply.github.com>
2023-07-15 11:58:24 +0200

    Get app running

--- script.js
+++ script.js
@@ -3,17 +3,49 @@ async function populate() {
   const request = new Request(requestURL);
 
   const response = await fetch(request);
-  const content = await response.json();
+  const projectsText = await response.text();
+  const content = JSON.parse(projectsText);
 
-  populateApp(content);
+  populateHeader(content);
+  populateProjects(content);
 }
 
-function populateApp(obj) {
-  const header = document.querySelector("header");
+function populateHeader(obj) {
+  const myHeader = document.querySelector("header");
   const myH1 = document.createElement("h1");
   myH1.textContent = obj.title;
-  header.appendChild(myH1);
+  myHeader.appendChild(myH1);
+}
+
+function populateProjects(obj) {
+
+  const section = document.querySelector("section");
+  const projects = obj.projects;
+
+  for (const project of projects) {
+  
+    const myArticle = document.createElement("article");
+    const myH2 = document.createElement("h2");
+    const myPara1 = document.createElement("p");
+    const myPara2 = document.createElement("p");
+    const img = document.createElement("img");
+    myPara1.className = "tech";
+    myPara2.className = "description";
+
+    myH2.textContent = project.name;
+    myPara1.textContent = `Tech used: ${project.tech}`;
+    myPara2.textContent = `Description: ${project.description}`;
+
+    var image = new Image();
+    image.src = project.image;
+
+    myArticle.appendChild(myH2);
+    myArticle.appendChild(image);
+    myArticle.appendChild(myPara1);
+    myArticle.appendChild(myPara2);
+    
+    section.appendChild(myArticle);
+  }
 }
 
 populate()
-
